ecf4d1e1a48068b580221bbb20165e95291b0c64
Feven7 <baejungmin077@gmail.com>
2023-12-27 10:05:54 +0900

    2s강
    CORS 설정 예제

--- project/src/main.ts
+++ project/src/main.ts
@@ -5,6 +5,17 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const corsOptions = {
+    // ex.
+    origin: "*",
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Origin',
+    preflightContinue: false,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  };
+
+  app.enableCors(corsOptions)
+
   app.use(helmet());
 
   await app.listen(3000);
