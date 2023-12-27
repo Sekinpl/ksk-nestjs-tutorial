294fda3c747c6c27a6226a8a14816af23723af0a
Feven7 <baejungmin077@gmail.com>
2023-12-27 10:22:42 +0900

    2s강
    winston 예제

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
+import * as winston from 'winston';
+import { utilities as nestWinstonModuleUtilities, WinstonModule, WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Module({
-  imports: [ApiModule],
+  imports: [
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike('ksk-nestjs-tutorial', { prettyPrint: true, colors: true }),
+          ),
+        }),
+      ],
+    }),
+    ApiModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- project/src/main.ts
+++ project/src/main.ts
@@ -1,9 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+// import * as winstonDaily from 'winston-daily-rotate-file'; // 이 부분은 로그를 기록하고 싶다면 주석을 해제해라.
+import * as winston from 'winston';
+import { utilities as nestWinstonModuleUtilities, WinstonModule, WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // Logger(Winston)
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike('MelodyWeb-Backend', { prettyPrint: true, colors: true }),
+          ),
+        }),
+        // 아래 부분은 모두 로그를 기록하고 싶을때 사용하는 코드이다. 만약에 로그를 기록하고 싶다면 주석을 해제하면 된다.
+        //  new winstonDaily(dailyOptions('info')),
+        //  new winstonDaily(dailyOptions('warn')),
+        //  new winstonDaily(dailyOptions('error')),
+      ],
+
+    })
+  });
+
+  // Logger(Winston) Settings
+
+  // 아래 주석 처리 된 부분은 로그를 파일로 기록하고 싶을때 사용하는 코드이다
+  //  const logDir = __dirname + '/../logs';
+  //  const dailyOptions = (level: string) => {
+  //    return {
+  //      level,
+  //      datePattern: 'YYYY-MM-DD HH:mm:ss',
+  //      dirname: logDir + `/${level}`,
+  //      filename: `%DATE%.${level}.log`,
+  //      maxFiles: 30,
+  //      zippedArchive: true,
+  //    };
+  //  };
+
+  // Using Logger(Winston) Globall
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   const corsOptions = {
     // ex.
